92cab48cd836921f16ad873d61b53d5860c53b62
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-12-16 11:47:37 -0300

    feat: req6 - Crie o endpoint PUT /talker/:id

---
+++ src/middleware/checkRateExists.js
@@ -0,0 +1,18 @@
+const checkRateExists = (req, res, next) => {
+  const { talk } = req.body;
+  const { rate } = talk;
+
+  if (rate === 0) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  if (!rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+
+  if (typeof rate !== 'number') {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' }); 
+  }
+
+  next();
+};
+
+module.exports = checkRateExists;
